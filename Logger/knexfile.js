448bd987ebe33eaf0be4df2350f918e064a6ee11
captainco <nguyenquocco@gmail.com>
2019-04-17 09:20:37 +0700

    Example about logger to write file, console and slack

---
+++ Logger/knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+    development: {
+        client     : 'mysql',
+        connection : {
+        host       : 'localhost',
+        user       : 'nodeApp',
+        password   : 'nodeApp',
+        database   : 'training',
+        charset    : 'utf8'
+      }
+    }
+
+};
