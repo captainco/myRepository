bf24166bbb9074b7f5e153f72374c8017119490f
captainco <nguyenquocco@gmail.com>
2019-04-18 08:40:15 +0700

    Example about logger to write file, console.log and slack

---
+++ 10. Logger/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    log: {
+
+        // Supporteds: file, console
+        using: process.env.LOG_ADAPTER,
+
+        adapters: {
+            file: {
+                path: "logs/system.log"
+            },
+
+            console: {
+
+            },
+
+            slack: {
+                slackWebHook: 'https://hooks.slack.com/services/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'
+            }
+        }
+    }
+}
