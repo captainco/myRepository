304b94519620298ef49c7f39da806a1ae983c741
captainco <48233630+captainco@users.noreply.github.com>
2019-04-18 08:31:31 +0700

    Example about calculator TDD

--- 10. Logger/getModule/router.js
+++ 10. Logger/getModule/router.js
@@ -6,9 +6,7 @@ const router        = new Router();
 
 router.get('/search/:name', async (context) => {
     let users    = await knex.select('*').from('users').where('username', 'like', '%' + context.params.keyword.toString() + '%');
-
     context.logger.log({query: context.params.name, result: users});
-
     context.body = nunjucks.render('index.html', { users });
 });
 
