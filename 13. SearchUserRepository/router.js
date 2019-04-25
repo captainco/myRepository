1a40b7ad7e212ce7aa8f62b540b3d11657f91033
captainco <nguyenquocco@gmail.com>
2019-04-25 11:40:48 +0700

    Example about search user repository

---
+++ 13. SearchUserRepository/router.js
@@ -0,0 +1,24 @@
+const Router        = require('koa-router');
+
+const router        = new Router();
+
+router.get('/search/:name', async (context) => {
+    let users    = await context.userRepository.getUser(context.params.name);
+
+    context.logger.log({query: context.params.name, result: users});
+    context.render('index.html', { users });
+});
+
+router.get('/add/:name', async (context) => {
+    let user = await context.userRepository.getUser(context.params.name);
+    if(user.length == 0) {
+        let addUser = await context.userRepository.addUser(context.params.name);
+        context.render('index.html', { addUser });
+        return addUser;
+    } else {
+        context.render('index.html', {message : 'User already exist'});
+    }
+});
+
+
+module.exports   = router;
